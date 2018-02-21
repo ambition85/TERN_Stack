1f2e2cced54afc369a8ecdd87ad83537df999d76
ambition85 <ambition9385@gmail.com>
2018-02-21 13:02:45 +0000

    Refactor unit test project

---
+++ UnitTest/tests/hello-world.spec.ts
@@ -0,0 +1,21 @@
+﻿import { expect } from 'chai';
+import 'mocha';
+
+describe('Hello function', () => {
+
+
+    it('should return hello world', () => {
+        console.log("Hello world test");
+
+        const result = 'Hello world!';//hello();
+        expect(result).to.equal('Hello world!');
+    });
+
+    it('should not return hello world', () => {
+        console.log("Not Hello world test");
+
+        const result = 'Hello world!';//hello();
+        expect(result).to.not.equal('Hello world!');
+    });
+
+});
---
+++ UnitTest/tests/user-rep.spec.ts
@@ -0,0 +1,20 @@
+﻿import UserRep from "../../Web/server/repository/user.rep";
+import { expect } from 'chai';
+import 'mocha';
+
+describe('User repository function', () => {
+
+    it('should return at least one user', () => {
+        console.log("Hello from user rep test");
+
+        const userRep: UserRep = new UserRep();
+
+        const users = userRep.read();
+
+        const numUsers = users.length;
+
+        console.log('Number of users retured by user rep: %i', numUsers);
+
+        expect(numUsers).above(0, 'Did not get any users returned.');
+    });
+});
